3e610a1f22f17f7d893dad4dbde19a30e36b2ccb
shortnd <ococncol@gmail.com>
2018-11-28 22:08:48 -0500

    added endpoint for homepage api

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -3,43 +3,66 @@
 namespace App\Http\Controllers;
 
 use App\Listing;
+use Illuminate\Http\Request;
 
 class ListingController extends Controller
 {
     public function get_listing($listing)
     {
         $model = $listing->toArray();
-        for ($i=1; $i < 4; $i++) { 
+        for ($i = 1; $i < 4; $i++) {
             $model['image_' . $i] = asset(
-                'images/'. $listing->id . '/Image_' . $i . '.jpg'
+                'images/' . $listing->id . '/Image_' . $i . '.jpg'
             );
         }
         return collect(['listing' => $model]);
     }
-    public function get_home_web()
+
+    public function add_meta_data($collection, $request)
+    {
+        return $collection->merge([
+            'path' => $request->getPathInfo()
+        ]);
+    }
+
+    public function get_listing_summaries()
     {
         $collection = Listing::all([
             'id', 'address', 'title', 'price_per_night'
         ]);
-        $collection->transform(function($listing) {
+        $collection->transform(function ($listing) {
             $listing->thumb = asset(
                 'images/' . $listing->id . '/Image_1_thumb.jpg'
             );
             return $listing;
         });
-        $data = collect(['listings' => $collection]);
+        return collect(['listings' => $collection]);
+    }
+
+    public function get_home_web(Request $request)
+    {
+        $data = $this->get_listing_summaries();
+        $data = $this->add_meta_data($data, $request);
         return view('app', ['data' => $data]);
     }
 
-    public function get_listing_web(Listing $listing)
+    public function get_home_api()
+    {
+        $data = $this->get_listing_summaries();
+        return response($data);
+    }
+
+    public function get_listing_web(Listing $listing, Request $request)
     {
         $data = $this->get_listing($listing);
+        $data = $this->add_meta_data($data, $request);
         return response($data);
     }
 
     public function get_listing_api(Listing $listing)
     {
         $data = $this->get_listing($listing);
+
         return response($data);
     }
 }
